94097859ed40d36e95e48955179c6af664a6eb62
pwang86 <pwang86@gmail.com>
2019-11-25 21:43:16 +1100

    add prod webpack

---
+++ webpack.prod.js
@@ -0,0 +1,48 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+const errorOverlayPlugin = new ErrorOverlayPlugin();
+
+module.exports = {
+  entry: "./src/index.js",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.jsx?$/,
+        use: ["eslint-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(jp(e*)g|png|gif|svg|pdf|ico)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin, errorOverlayPlugin]
+};
