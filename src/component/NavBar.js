cf367c72b758d980cb7261c8755a0edfc95af99c
pwang86 <pwang86@gmail.com>
2019-11-27 21:52:21 +1100

    add bootstrap navbar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,17 +1,13 @@
 import React from "react";
-function NavBar(){
-return(
-    <nav>
-        <ul className="navbar--menu">
-            <li className="navbar--menu-list">
-                Introduction
-            </li>
-            <li className="navbar-menu-list">
-                Contact
-            </li>
-        </ul>
+function NavBar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <ul className="navbar--menu">
+        <li className="navbar--menu-list">Introduction</li>
+        <li className="navbar-menu-list">Contact</li>
+      </ul>
     </nav>
-);
+  );
 }
 
-export default NavBar；
+export default NavBar;
