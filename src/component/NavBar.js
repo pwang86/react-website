6ace05bcfa5e7f4c1e75ebae8b03310dbccbe14c
pwang86 <pwang86@gmail.com>
2019-11-27 17:22:19 +1100

    add navbar init

---
+++ src/component/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+function NavBar(){
+return(
+    <nav>
+        <ul className="navbar--menu">
+            <li className="navbar--menu-list">
+                Introduction
+            </li>
+            <li className="navbar-menu-list">
+                Contact
+            </li>
+        </ul>
+    </nav>
+);
+}
+
+export default NavBar；
