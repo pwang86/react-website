47558841842076d25aacbb71bdd97db955492415
pwang86 <pwang86@gmail.com>
2019-12-01 22:24:07 +1100

    add english and chinese button in navbar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,9 +1,9 @@
 import React from "react";
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div>
-        <ul className="navbar-nav">
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav mr-auto">
           <li className="nav-item">
             <a className="nav-link" href="#">
               Introduction
@@ -15,6 +15,8 @@ function NavBar() {
             </a>
           </li>
         </ul>
+        <span className="navbar-text">English</span>
+        <span className="navbar-text">中文</span>
       </div>
     </nav>
   );
