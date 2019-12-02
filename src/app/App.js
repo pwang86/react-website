0aa23596a896e083cc4cc64274953870e259bcdd
pwang86 <pwang86@gmail.com>
2019-12-02 11:40:38 +1100

    add contact form

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import NavBar from "../component/NavBar";
+import Contact from "../component/Contact";
 function App() {
-  return <NavBar />;
+  return (
+    <div>
+      <NavBar />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
