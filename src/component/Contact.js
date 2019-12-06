18d5146e6ec6b2eca3cb90d7911b3225e9b5582c
pwang86 <pwang86@gmail.com>
2019-12-06 11:58:03 +1100

    modify placeholder

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -11,25 +11,25 @@ function Contact() {
         <div className="form-group row">
           <label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" id="inputName" palceholder="Name" />
+            <input type="text" className="form-control" id="inputName" placeholder="Name" />
           </div>
         </div>
         <div className="form-group row">
           <label htmlFor="inputEmail" className="col-sm-2 col-form-label">Email</label>
           <div className="col-sm-10">
-            <input type="email" className="form-control" id="inputEmail" palceholder="Email" />
+            <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
           </div>
         </div>
         <div className="form-group row">
           <label htmlFor="inputSubject" className="col-sm-2 col-form-label">Subject</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" id="inputSubject" palceholder="Subject" />
+            <input type="text" className="form-control" id="inputSubject" placeholder="Subject" />
           </div>
         </div>
         <div className="form-group row">
           <label htmlFor="inputMessage" className="col-sm-2 col-form-label">Message</label>
           <div className="col-sm-10">
-            <textarea className="form-control" id="inputMessage" rows="6" palceholder="Message" />
+            <textarea className="form-control" id="inputMessage" rows="6" placeholder="Message" />
           </div>
         </div>
         <div className="form-group row d-flex justify-content-center">
