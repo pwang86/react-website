2f2cf5851e82242c26856d89cac3401dba91579c
pwang86 <pwang86@gmail.com>
2019-12-07 21:53:26 +1100

    add home and introduction component

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import NavBar from "../component/NavBar";
 import Contact from "../component/Contact";
+import Home from "../component/Home";
+import Introduction from "../component/Introduction";
+
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <Introduction />
       <Contact />
     </div>
   );
