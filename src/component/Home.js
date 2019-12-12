c72e19a4c7d17429c7653ce82d159d15604a5c72
pwang86 <pwang86@gmail.com>
2019-12-12 22:04:05 +1100

    add image url

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Home.css";
+import BgImg from "../images/test.jpg";
+
+const homeImg = {
+  backgroundImage: "url(" + BgImg + ")"
+};
 
 function Home() {
   return (
-    <div className="home">
+    <div className="home" style={homeImg}>
       <span>Home</span>
     </div>
   );
