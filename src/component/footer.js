f7b7e859f26d2d6ebc85ea8fed98c5110fa154df
pwang86 <pwang86@gmail.com>
2019-12-16 21:08:11 +1100

    add ul into footer

---
+++ src/component/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <ul>
+        <li>Instagram</li>
+      </ul>
+      <div>Date</div>
+    </div>
+  );
+}
+
+export default Footer;
