45e68aca50944e19739f025819b796d2456e930c
pwang86 <pwang86@gmail.com>
2019-12-19 16:36:38 +1100

    import introduction.css

--- src/component/Introduction.js
+++ src/component/Introduction.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "./Introduction.css";
 
 function Introduction() {
   return (
     <div className="introduction">
-      <span>Introduction</span>
+      <span>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum. 
+        Fusce blandit ultrices sapien, in accumsan orci rhoncus eu. Sed sodales venenatis arcu, 
+        id varius justo euismod in. Curabitur egestas consectetur magna vitae.
+      </span>
     </div>
   );
 }
