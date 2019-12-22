4357b0767ad055ad50bfb3bb71d5954c0ded4289
pwang86 <pwang86@gmail.com>
2019-12-22 19:03:17 +1100

    add footer

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,7 @@ import NavBar from "../component/NavBar";
 import Contact from "../component/Contact";
 import Home from "../component/Home";
 import Introduction from "../component/Introduction";
+import Footer from "../component/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <Introduction />
       <Contact />
+      <Footer />
     </div>
   );
 }
