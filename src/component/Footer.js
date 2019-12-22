86d2bef30dd59f57a004fa22397fe76c061ff35e
pwang86 <pwang86@gmail.com>
2019-12-22 19:03:48 +1100

    add footer content

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,1 +1,14 @@
+import React from "react";
 import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer--section text-center">
+      <div>
+        <p>© Joni's Driving School</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
