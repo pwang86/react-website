78d48bfa5297d568500de8e1a2ca9285df6595a4
pwang86 <pwang86@gmail.com>
2019-12-25 22:39:43 +1100

    init i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,2 +1,27 @@
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import Backend from "i18next-xhr-backend";
+import { reactI18nextModule } from "react-i18next";
+
+i18n
+  .use(Backend)
+  .use(reactI18nextModule)
+  .init({
+    lng: "en",
+    backend: {
+      loadPath: "{{ns}}/{{lng}}.json"
+    },
+    fallbackLng: "en",
+    debug: true,
+    ns: ["translations"],
+    defaultNS: "translations",
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ","
+    },
+    react: {
+      wait: true
+    }
+  });
+
+export default i18n;
