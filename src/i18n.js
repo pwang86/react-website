8d7e08c89df98657d101a7f364620afae1f39865
pwang86 <pwang86@gmail.com>
2019-12-26 11:05:26 +1100

    use resoources instead of backend

--- src/i18n.js
+++ src/i18n.js
@@ -1,27 +1,53 @@
 import i18n from "i18next";
-import Backend from "i18next-xhr-backend";
+// import Backend from "i18next-xhr-backend";
 import { reactI18nextModule } from "react-i18next";
+import translationEN from "../public/locales/en/translation.json";
+import translationZH from "../public/locales/zh/translation.json";
 
-i18n
-  .use(Backend)
-  .use(reactI18nextModule)
-  .init({
-    lng: "en",
-    backend: {
-      loadPath: "{{ns}}/{{lng}}.json"
-    },
-    fallbackLng: "en",
-    debug: true,
-    ns: ["translations"],
-    defaultNS: "translations",
-    keySeparator: false,
-    interpolation: {
-      escapeValue: false,
-      formatSeparator: ","
-    },
-    react: {
-      wait: true
-    }
-  });
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  zh: {
+    translation: translationZH
+  }
+};
+
+i18n.use(reactI18nextModule).init({
+  resources,
+  lng: "en",
+  fallbackLng: "en",
+  debug: true,
+  keySeparator: false,
+  interpolation: {
+    escapeValue: false,
+    formatSeparator: ","
+  },
+  react: {
+    wait: true
+  }
+});
+
+// i18n
+//   .use(Backend)
+//   .use(reactI18nextModule)
+//   .init({
+//     lng: "en",
+//     backend: {
+//       loadPath: "/locales/{{lng}}/{{ns}}.json"
+//     },
+//     fallbackLng: "en",
+//     debug: true,
+//     ns: ["translation"],
+//     defaultNS: "translation",
+//     keySeparator: false,
+//     interpolation: {
+//       escapeValue: false,
+//       formatSeparator: ","
+//     },
+//     react: {
+//       wait: true
+//     }
+//   });
 
 export default i18n;
