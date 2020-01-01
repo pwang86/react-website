493743b4bf31f637274d3ac69b1cb2f3b0f5f0ca
pwang86 <pwang86@gmail.com>
2020-01-02 07:41:11 +1100

    add reaci18next and title for introduction

--- src/component/Introduction.js
+++ src/component/Introduction.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { withNamespaces } from "react-i18next";
 import "./Introduction.css";
 import feature1 from "../images/book.jpg";
 import feature2 from "../images/penguin.jpg";
 import feature3 from "../images/train.jpg";
 
-function Introduction() {
+function Introduction({ t }) {
   return (
     <div className="introduction">
+      <h2 className="text-center">{t("title")}</h2>
       <section className="feature left">
         <a href="#" className="feature-image">
           <img src={feature1} alt="book" />
@@ -53,4 +55,4 @@ function Introduction() {
   );
 }
 
-export default Introduction;
+export default withNamespaces()(Introduction);
