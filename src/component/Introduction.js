6fddae4d993866dce9941329081e656c198c1547
pwang86 <pwang86@gmail.com>
2020-01-17 22:10:01 +1100

    add interactive info in introduction

--- src/component/Introduction.js
+++ src/component/Introduction.js
@@ -38,12 +38,7 @@ function Introduction({ t }) {
         </a>
         <div className="feature-content">
           <h4>{t("interactive")}</h4>
-          <p>
-            Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore
-            condimentum. Fusce blandit ultrices sapien, in accumsan orci rhoncus
-            eu. Sed sodales venenatis arcu, id varius justo euismod in.
-            Curabitur egestas consectetur magna vitae.
-          </p>
+          <p>{t("interactive-info")}</p>
         </div>
       </section>
     </div>
