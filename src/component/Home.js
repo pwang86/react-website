a2b5d392b39433ee8f9cc5642d70c40670ef69cb
pwang86 <pwang86@gmail.com>
2020-01-22 22:06:10 +1100

    set translate for driving school

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,20 +1,19 @@
 import React from "react";
 import "./Home.css";
 import BgImg from "../images/test.jpg";
+import { withNamespaces } from "react-i18next";
 
 const homeImg = {
   backgroundImage: "url(" + BgImg + ")"
 };
 
-function Home() {
+function Home({ t }) {
   return (
     <div className="home" style={homeImg}>
       <p className="animated fadeInDown delay-2s text layer1">Joni's</p>
-      <h3 className="animated fadeInLeft delay-3s text layer2">
-        Driving School
-      </h3>
+      <h3 className="animated fadeInLeft delay-3s text layer2">{t("ds")}</h3>
     </div>
   );
 }
 
-export default Home;
+export default withNamespaces()(Home);
