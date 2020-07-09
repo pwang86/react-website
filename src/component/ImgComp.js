f41dec7d9d27207d31e3a9926f2c98017c1b45bf
pwang86 <pwang86@gmail.com>
2020-07-10 07:46:49 +1000

    import image css

--- src/component/ImgComp.js
+++ src/component/ImgComp.js
@@ -1,12 +1,8 @@
 import React from "react";
+import "./ImgComp.css";
 
 function ImgComp({ src }) {
-  let imgStyles = {
-    width: 100 + "%",
-    height: "auto",
-  };
-
-  return <img src={src} alt="slide-img" style={imgStyles} />;
+  return <img src={src} alt="slide-img" className="imgStyles" />;
 }
 
 export default ImgComp;
