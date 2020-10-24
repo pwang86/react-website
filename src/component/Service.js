aac7cf77b5a3480c89e960f6b12f59425b9760ae
pwang86 <pwang86@gmail.com>
2020-10-24 20:06:20 +1100

    add title in service

--- src/component/Service.js
+++ src/component/Service.js
@@ -4,7 +4,8 @@ import { withNamespaces } from "react-i18next";
 
 function Service({t}) {
   return (
-    <div>
+    <div className="service">
+      <h1 className="text-center">{t("service")}</h1>
       <li>Driving Test</li>
       <li>Refresh</li>
       <li>Moving to NSW</li>
