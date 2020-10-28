85cf437fea67536c291f2a59eda34270086f0c79
pwang86 <pwang86@gmail.com>
2020-10-28 16:08:32 +1100

    hover on card

--- src/component/Service.js
+++ src/component/Service.js
@@ -9,24 +9,24 @@ function Service({t}) {
       <div className="container">
         <div className="row service-row">
           <div className="col-sm-4">
-            <div className="card box-shadow">
-              <h5 className="card-header">{t("service1")}</h5>
+            <div className="card service-card">
+              <h5 className="card-header card-header--bg text-white">{t("service1")}</h5>
               <div className="card-body">
                 <p className="card-text">{t("service1-info")}</p>
               </div>
             </div>
           </div>
           <div className="col-sm-4">
-            <div className="card box-shadow">
-              <h5 className="card-header">{t("service2")}</h5>
+            <div className="card service-card">
+              <h5 className="card-header card-header--bg text-white">{t("service2")}</h5>
               <div className="card-body">
                 <p className="card-text">{t("service1-info")}</p>
               </div>
             </div>
           </div>
           <div className="col-sm-4">
-            <div className="card box-shadow">
-              <h5 className="card-header">{t("service3")}</h5>
+            <div className="card service-card">
+              <h5 className="card-header card-header--bg text-white">{t("service3")}</h5>
               <div className="card-body">
                 <p className="card-text">{t("service1-info")}</p>
               </div>
