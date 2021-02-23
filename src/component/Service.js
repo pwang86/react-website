64a633bed92605d4e76b6fdec5ced0db9565ad57
pwang86 <pwang86@gmail.com>
2021-02-23 16:47:22 +1100

    cards with same height

--- src/component/Service.js
+++ src/component/Service.js
@@ -6,76 +6,27 @@ function Service({t}) {
   return (
     <div className="service text-center">
       <h1 className="service-header">{t("service")}</h1>
-      <div className="container">
-        <div className="row service-row">
-          <div className="col-sm-4">
-            <div className="card service-card">
-              <h5 className="card-header card-header--bg text-white">{t("service1")}</h5>
-              <div className="card-body">
-                <p className="card-text">{t("service1-info")}</p>
-                <p>
-                  <ul>
-                    <li>Bankstown</li>
-                    <li>Silverwater</li>
-                  </ul>
-                </p>
-              </div>
-            </div>
+      <div className="card-deck">
+        <div className="card">
+          <div className="card-body card-body-custom">
+            <h5 className="card-title card-title-custom text-white">{t("service1")}</h5>
+            <p className="card-text card-text-custom">{t("service1-info")}</p>
           </div>
-          <div className="col-sm-4">
-            <div className="card service-card">
-              <h5 className="card-header card-header--bg text-white">{t("service2")}</h5>
-              <div className="card-body">
-                <p className="card-text">{t("service2-info")}</p>
-              </div>
-            </div>
+        </div>
+        <div className="card">
+          <div className="card-body card-body-custom">
+            <h5 className="card-title card-title-custom text-white">{t("service2")}</h5>
+            <p className="card-text card-text-custom">{t("service2-info")}</p>
           </div>
-          <div className="col-sm-4">
-            <div className="card service-card">
-              <h5 className="card-header card-header--bg text-white">{t("service3")}</h5>
-              <div className="card-body">
-                <p className="card-text">{t("service3-info")}</p>
-              </div>
-            </div>
+        </div>
+        <div className="card">
+          <div className="card-body card-body-custom">
+            <h5 className="card-title card-title-custom text-white">{t("service3")}</h5>
+            <p className="card-text card-text-custom">{t("service3-info")}</p>
           </div>
         </div>
       </div>
     </div>
-);
+  );
 }
-
-
-
 export default withNamespaces()(Service);
-/*
-
-      */
-
-      /*
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="card box-shadow">
-            <h5 className="card-header">{t("service1")}</h5>
-            <div className="card-body">
-              <p className="card-text">{t("service1-info")}</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-4">
-          <div className="card box-shadow">
-            <h5 className="card-header">{t("service2")}</h5>
-            <div className="card-body">
-              <p className="card-text">{t("service1-info")}</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-4">
-          <div className="card box-shadow">
-            <h5 className="card-header">{t("service3")}</h5>
-            <div className="card-body">
-              <p className="card-text">{t("service1-info")}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      */
