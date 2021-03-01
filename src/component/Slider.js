cb55bb08439af9c6299e95a8fb54452c704c0db4
pwang86 <pwang86@gmail.com>
2021-03-01 22:24:57 +1100

    use bootstrap carousel

--- src/component/Slider.js
+++ src/component/Slider.js
@@ -1,66 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Slider.scss";
-import ImgComp from "./ImgComp";
-import Dot from "./Dot";
 import s1 from "../images/L.jpg";
 import s2 from "../images/window.jpg";
 import s3 from "../images/mirror.jpg";
 
 function Slider() {
-  let sliderArr = [
-    <ImgComp src={s1} />,
-    <ImgComp src={s2} />,
-    <ImgComp src={s3} />,
-  ];
-
-  const [x, setX] = useState(0);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      goRight();
-    }, 4000);
-    return () => clearInterval(interval);
-  }, [x]);
-  const goLeft = () => {
-    x === 0 ? setX(-100 * (sliderArr.length - 1)) : setX(x + 100);
-  };
-  const goRight = () => {
-    x === -100 * (sliderArr.length - 1) ? setX(0) : setX(x - 100);
-  };
-
-  let dotArr = [
-    <Dot dotId="0" slideId={`${x}`} />,
-    <Dot dotId="1" slideId={`${x}`} />,
-    <Dot dotId="2" slideId={`${x}`} />,
-  ];
-
   return (
-    <div className="home slider">
+    <div id="carouselIndicators" className="carousel slide slide-custom" data-ride="carousel">
       <div className="overlay" />
-      {sliderArr.map((item, index) => {
-        return (
-          <div
-            key={index}
-            className="slide"
-            style={{ transform: `translateX(${x}%)` }}
-          >
-            {item}
-          </div>
-        );
-      })}
-      <button id="goLeft" onClick={goLeft}>
-        <i className="fas fa-chevron-left" />
-      </button>
-      <button id="goRight" onClick={goRight}>
-        <i className="fas fa-chevron-right" />
-      </button>
-
-      <div className="row">
-        {dotArr.map((item, index) => {
-          return <div key={index}>{item}</div>;
-        })}
+      <ol className="carousel-indicators">
+        <li data-target="#carouselIndicators" data-slide-to="0" className="active" />
+        <li data-target="#carouselIndicators" data-slide-to="1" />
+        <li data-target="#carouselIndicators" data-slide-to="2" />
+      </ol>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={s1} className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src={s2} className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src={s3} className="d-block w-100" alt="..." />
+        </div>
       </div>
+      <a className="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true" />
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true" />
+        <span className="sr-only">Next</span>
+      </a>
     </div>
   );
 }
-/* style={{background: `(-index * 100 === ${x} ? "white" : "black")`}} */
+
 export default Slider;
