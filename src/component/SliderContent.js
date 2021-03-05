393aa8d92f91bf48ec14e72825e3f9ca2884a1d2
pwang86 <pwang86@gmail.com>
2021-03-05 23:03:11 +1100

    use const to replace function

--- src/component/SliderContent.js
+++ src/component/SliderContent.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./SliderContent.scss";
 
-function SliderContent(props) {
+const SliderContent = props => {
   return(
-    <div className="slider-content" style={{transform:`translateX(-${props.translate}%)`, transition:`transform ease-out ${props.transition}s`}} />
+    <div className="slider-content" style={{transform:`translateX(-${props.translate}%)`, transition:`transform ease-out ${props.transition}s`, width: `${props.width}px`}} />
   );
 };
 
