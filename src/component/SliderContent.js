d66c904c18ffef3f88d2a456958eb3accbdd6b62
pwang86 <pwang86@gmail.com>
2021-03-06 23:38:40 +1100

    use px to replace %

--- src/component/SliderContent.js
+++ src/component/SliderContent.js
@@ -3,7 +3,7 @@ import "./SliderContent.scss";
 
 const SliderContent = props => {
   return(
-    <div className="slider-content" style={{transform:`translateX(-${props.translate}%)`, transition:`transform ease-out ${props.transition}s`, width: `${props.width}px`}} />
+    <div className="slider-content" style={{transform:`translateX(-${props.translate}px)`, transition:`transform ease-out ${props.transition}s`, width: `${props.width}px`}} />
   );
 };
 
