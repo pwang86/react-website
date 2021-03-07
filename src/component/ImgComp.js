1f8000bd1e79513fe2db508bd0fce65fada71990
pwang86 <pwang86@gmail.com>
2021-03-07 17:22:24 +1100

    use url to replace src

--- src/component/ImgComp.js
+++ src/component/ImgComp.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./ImgComp.scss";
 
-function ImgComp({ src }) {
-  return <img src={src} alt="slide-img" className="imgStyles" />;
+function ImgComp({ url }) {
+  return <img src={url} alt="slide-img" className="imgStyles" />;
 }
 
 export default ImgComp;
