41035806407f4c07e51c683f2a6c78b3dca62ed4
pwang86 <pwang86@gmail.com>
2021-03-14 10:14:53 +1100

    rename slider2 to slider

--- src/component/Slider.js
+++ src/component/Slider.js
@@ -1,37 +1,141 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 import "./Slider.scss";
+import SlideImg from "./SlideImg";
+import SliderContent from "./SliderContent";
+import Dots from "./Dots";
 import s1 from "../images/L.jpg";
 import s2 from "../images/window.jpg";
 import s3 from "../images/mirror.jpg";
 
-function Slider() {
+const getWidth = () => window.innerWidth;
+
+const Slider = () => {
+
+  const imgsArr = [s1, s2, s3];;
+  const firstSlide = imgsArr[0];
+  const secondSlide = imgsArr[1];
+  const thirdSlide = imgsArr[2]; 
+
+  const[state, setState] = useState({
+    translate: getWidth(),
+    transition: 0.45,
+    activeIndex: 0,
+    _slides: [thirdSlide, firstSlide, secondSlide]
+  });
+  const {translate, transition, activeIndex, _slides} = state;
+  const autoPlayRef = useRef();
+  const transitionRef = useRef();
+  const resizeRef = useRef();
+
+  useEffect(() => {
+    autoPlayRef.current = goRight;
+    transitionRef.current = smoothTransition;
+    resizeRef.current = handleResize;
+  });
+
+  useEffect(() => {
+    const play = () => {
+      autoPlayRef.current();
+    }
+
+    const smooth = () => {
+      transitionRef.current();
+    }
+
+    const resize = () => {
+      resizeRef.current();
+    }
+
+    const transitionEnd = window.addEventListener('transitionend', smooth);
+    const onResize = window.addEventListener('resize', resize);
+    const interval = setInterval(play, 3000);
+    return () => {
+      window.removeEventListener('transitionend', transitionEnd);
+      window.removeEventListener('resize', onResize);
+      clearInterval(interval);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (transition === 0) {
+      setState({...state, transition: 0.45});
+    }
+  }, [transition]);
+
+  const handleResize = () => {
+    setState({...state, translate: getWidth(), transition: 0});
+  }
+
+  const smoothTransition = () => {
+    let _slides = [];
+    
+    if (activeIndex === imgsArr.length - 1)
+      _slides = [secondSlide, thirdSlide, firstSlide];
+    else if (activeIndex === 0) 
+      _slides = [thirdSlide, firstSlide, secondSlide];
+    else
+      _slides = imgsArr.slice(activeIndex - 1, activeIndex + 2);
+
+    setState({
+      ...state,
+      _slides,
+      transition:0,
+      translate: getWidth()
+    });
+  }
+
+  const goRight = () => {
+    setState({
+      ...state,
+      translate: translate + getWidth(),
+      activeIndex: activeIndex === imgsArr.length - 1 ? 0 : activeIndex + 1
+    });
+  }
+
+  const goLeft = () => {
+    setState({
+      ...state,
+      translate: 0,
+      activeIndex: activeIndex === 0 ? imgsArr.length - 1 : activeIndex - 1
+    });
+  }
+
+
+/*
+  let dotArr = [
+    <Dot dotId="0" slideId={`${slideIndex}`} />,
+    <Dot dotId="1" slideId={`${slideIndex}`} />,
+    <Dot dotId="2" slideId={`${slideIndex}`} />,
+  ];
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      showSlides();
+    }, 4000);
+    return () => clearInterval(interval);
+  }, [slideIndex]);
+
+  const showSlides = () => {
+    slideIndex === (sliderArr.length - 1) ? setSlideIndex(0): setSlideIndex(slideIndex + 1);
+  };
+*/
   return (
-    <div id="carouselIndicators" className="carousel slide slide-custom" data-ride="carousel">
+    <div className="home slider">
       <div className="overlay" />
-      <ol className="carousel-indicators">
-        <li data-target="#carouselIndicators" data-slide-to="0" className="active" />
-        <li data-target="#carouselIndicators" data-slide-to="1" />
-        <li data-target="#carouselIndicators" data-slide-to="2" />
-      </ol>
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src={s1} className="d-block w-100" alt="..." />
-        </div>
-        <div className="carousel-item">
-          <img src={s2} className="d-block w-100" alt="..." />
-        </div>
-        <div className="carousel-item">
-          <img src={s3} className="d-block w-100" alt="..." />
-        </div>
-      </div>
-      <a className="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true" />
-        <span className="sr-only">Previous</span>
-      </a>
-      <a className="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true" />
-        <span className="sr-only">Next</span>
-      </a>
+      <SliderContent translate={translate} transition={transition} width={getWidth() * _slides.length}>
+        {_slides.map((slide, index) => {
+          return (<SlideImg width={getWidth()} key={index} content={slide} />);
+        })}
+      </SliderContent>
+      
+      <button id="goLeft" onClick={goLeft}>
+        <i className="fas fa-chevron-left" />
+      </button>
+      <button id="goRight" onClick={goRight}>
+        <i className="fas fa-chevron-right" />
+      </button>
+
+      <Dots slides={imgsArr} activeIndex={activeIndex} />
     </div>
   );
 }
