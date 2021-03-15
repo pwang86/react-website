d4cc22100955bc46d931c66554ff72bc452b5aab
pwang86 <pwang86@gmail.com>
2021-03-15 13:46:55 +1100

    delete beta slider component

--- src/component/ImgComp.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ImgComp.scss";
-
-function ImgComp(props) {
-  return <img src={props.content} alt="slide-img" className="imgStyles" />;
-}
-
-export default ImgComp;
